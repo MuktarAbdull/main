c7b9432060f59ec24d716f5e1fcffd489ca0b2e4
Muktar Abdullahi Ommar <144605491+MuktarAbdull@users.noreply.github.com>
2023-12-16 09:52:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+            //ARRAYS
+    // let fruits  = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange']
+    //         console.log(fruits)
+
+                    //ARRAY LENGTH
+/*
+    let fruits  = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange']
+    console.log(fruits.length)
+    */
+                    //ARRAY INDEX
+        /*
+    let fruits  = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange']
+    console.log(fruits[2])
+    */
+               // ADDING OR REMOOVING FROM ARRAY
+                    //PUSH AND POP
+                    let fruits  = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange', 'Lows']
+                     console.log(fruits)
+                        //ADDING AT THE BACK
+                     fruits.push('Lemon')
+                     console.log(fruits)
+                         //REMOVING AT THE BACK
+                     fruits.pop()
+                     console.log(fruits)
+                        //ADDING AT THE BEGINING
+                    fruits.unshift('Strewbary');
+                    console.log(fruits)
+                            //REMOVING AT THE BEGINING
+                    fruits.shift()
+                    // fruits.shift([0])
+                    console.log(fruits)
+            //REMOVING SPICIFIC ONE
+    let pos = fruits.indexOf('Banana')
+    let removing = fruits.splice(pos,1)
+    console.log(fruits)
+                //LOOP THROUGH ARRAY
+    fruits.forEach(function (items) {
+document.getElementById("demo")
+            console.log(items)
+    })
+
+    const people = 
+    ['Muktar',
+    'Abdurahim',
+    'Abdisalam',
+    'Nasteho',
+    'Ahmed',
+    'Ayaan'
+];
+
+console.log(people)
+
+let names = [
+    'muktar',
+    'abdisalam',
+    'mahamed',
+    'ali',
+    'Osman',
+    'Iman'
+];
+names.forEach(function(students){
+
+console.log(students)
+});
+
+let Ardayda =[
+'ayaan',
+'deeqa',
+'nastexo',
+'muna',
+'mukhtaar',
+'muuse',
+'faarax'
+];
+Ardayda.forEach(function(tiro){
+    console.log(tiro)
+})
+
